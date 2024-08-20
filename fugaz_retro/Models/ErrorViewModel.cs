b9597e676e75429617e8c9a0c4bea61db7d1da07
NeiHiguita <neiderhiguita13@gmail.com>
2024-08-20 15:28:13 -0500

    Revert "subir proyecto"
    This reverts commit d9079cc0e7ce3a984b56f7f7dbb424a42fb438ac.

--- fugaz_retro/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace fugaz_retro.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
